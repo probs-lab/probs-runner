71bf34320102f9603d25d34f03fcab4ef0e3f40c
Rick Lupton <mail@ricklupton.name>
2023-08-05 08:54:45 +0100

    PROBS namespace has "#" not "/" at end

--- src/probs_runner/namespace.py
+++ src/probs_runner/namespace.py
@@ -4,7 +4,7 @@ from rdflib import Namespace
 from rdflib.namespace import RDF, RDFS
 
 
-PROBS = Namespace("http://w3id.org/probs-lab/ontology/")
+PROBS = Namespace("http://w3id.org/probs-lab/ontology#")
 QUANTITYKIND = Namespace("http://qudt.org/vocab/quantitykind/")
 PROV = Namespace("http://www.w3.org/ns/prov#")
 
