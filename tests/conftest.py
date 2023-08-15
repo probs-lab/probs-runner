3ddadd094d31ea5d3a62f5d1aae39509e78f3fbd
Rick Lupton <mail@ricklupton.name>
2023-08-15 12:27:57 +0100

    minor: typo in error message

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ def script_source_dir():
     if "PROBS_SCRIPT_SOURCE_DIR" in os.environ:
         script_source_dir = Path(os.environ["PROBS_SCRIPT_SOURCE_DIR"])
         if not script_source_dir.exists():
-            raise FileNotFoundError(f"SCRIPT_SOURCE_DIR not found: '{script_source_dir}'")
+            raise FileNotFoundError(f"PROBS_SCRIPT_SOURCE_DIR not found: '{script_source_dir}'")
     else:
         script_source_dir = None
     return script_source_dir
